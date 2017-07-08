246c83ebe536a029194e6bdae48cf66802889548
Temi Lajumoke <temi@temilajumoke.com>
2017-07-08 15:32:40 +0100

    setup app folder structure

---
+++ FizzBuzzLibrary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FizzBuzzLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
